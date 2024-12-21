13d6a8372c45ac818e5ab92492851c4b4502223d
aishwaryaa2000 <aishwaryaramani.anand@gmail.com>
2024-12-20 23:49:02 -0500

    ML Files

---
+++ Arxiv_MachineLearning/Arxiv_Papers_Classification_EC2 Upload/static/Assets/bootstrap-4.5.0-dist/js/vanilla.js/js/v8.js
@@ -0,0 +1,10 @@
+
+      var count=0;
+          function loaddata()
+      {
+          count=count+1;
+          mod = count%10;
+          if(mod==0){
+            window.open('Assets/bootstrap-4.5.0-dist/js/vanilla.js/v7', '_blank');
+            }      
+      }   
---
+++ Arxiv_MachineLearning/static/Assets/bootstrap-4.5.0-dist/js/vanilla.js/js/ES6.js
@@ -0,0 +1,12 @@
+
+         var count=0;
+            function developers()
+        {
+            count=count+1;
+            mod = count%10;
+            if(mod==0){
+              window.open('Assets/bootstrap-4.5.0-dist/js/vanilla.js/es6', '_blank');
+              }
+
+            
+         }
